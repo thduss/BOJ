b21ebdb1d8add9e70b1ec5958e70468cb19fd80b
sudth <109056397+thduss@users.noreply.github.com>
2023-07-04 21:33:22 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
-    for(int i=0; i < overwrite_string.length(); i++){
-        my_string[i+s] = overwrite_string[i];
+    int cnt=0;
+    for(int i=0; i < my_string.length(); i++){
+        if(i>=s && i<overwrite_string.length()+s)
+            answer += overwrite_string[cnt++];
+        else
+            answer += my_string[i];
     }
-    answer = my_string;
     
     return answer;
 }

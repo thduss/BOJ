c7f6102cd46e9d66a549ddfde32c5d1fc6296171
thdus <109056397+thduss@users.noreply.github.com>
2024-01-04 16:25:37 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+int main()
+{   
+    map<int, int> m;
+    int maxNum = 0;
+    int index = 0;
+
+    for(int i=1; i<=9; i++){
+        int value;
+        cin >> value;
+        m.insert(pair(i, value));
+    }
+
+    for(int i=1; i<=9; i++){
+        maxNum = max(maxNum, m[i]);
+        if(maxNum == m[i]){
+            index = i;
+        }
+    }
+
+    cout << maxNum << endl << index;
+}   

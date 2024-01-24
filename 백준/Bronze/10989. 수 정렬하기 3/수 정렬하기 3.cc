f601e408b939ae64606057d8dfcbf2fa746e5d6f
thdus <109056397+thduss@users.noreply.github.com>
2024-01-24 17:01:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2056 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, arr[10001]={0,};
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        arr[a] += 1;
+    }
+
+    for (int i=1; i<10001;i++) {
+        for (int j=0; j<arr[i]; j++) {
+            cout << i << '\n';
+        }
+    }
+
+}

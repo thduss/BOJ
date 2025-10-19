6407f1f39f796492d7c7d910a090809bd5fef127
Soyeon Kim <109056397+thduss@users.noreply.github.com>
2025-10-19 15:25:07 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int y = Integer.parseInt(br.readLine());
+		
+		System.out.println(y-543);
+	}
+}

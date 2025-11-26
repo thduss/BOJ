b517ab9d51c311494aa4055645ed7851724c8f7a
Soyeon Kim <109056397+thduss@users.noreply.github.com>
2025-11-26 12:43:15 +0900

    [Silver I] Title: 점프, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,36 +1,43 @@
-import java.util.*;
 import java.io.*;
-public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][N];
-		 
-		for (int i = 0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j<N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+import java.util.*;
 
-		long[][] dp = new long[N][N];
-		dp[0][0] = 1;
-		
-		for (int i = 0; i<N; i++) {
-			for (int j = 0; j<N; j++) {
-				if (arr[i][j] == 0) continue;
-				if (i==N-1 && j==N-1) continue;
-				
-				int v = arr[i][j];
-				int nx = v+i;
-				int ny = v+j;
-				
-				if (nx<N) dp[nx][j] += dp[i][j];
-				if (ny<N) dp[i][ny] += dp[i][j];
-			}
-		}
-				
-		System.out.println(dp[N-1][N-1]);
-	}
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        long[][] dp = new long[N][N];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == N - 1 && j == N - 1)
+                    break;
+
+                if (dp[i][j] != 0) {
+                    int num = map[i][j];
+
+                    if (i + num < N) {
+                        dp[i + num][j] += dp[i][j];
+                    }
+
+                    if (j + num < N) {
+                        dp[i][j + num] += dp[i][j];
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[N - 1][N - 1]);
+    }
 }

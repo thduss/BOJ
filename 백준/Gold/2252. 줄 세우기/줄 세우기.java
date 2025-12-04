c8146421755e5cd8a31c82730674242bff449cd0
Soyeon Kim <109056397+thduss@users.noreply.github.com>
2025-12-04 09:39:18 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 45504 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,41 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		int N = Integer.parseInt(input[0]);
-		int M = Integer.parseInt(input[1]);
+		ArrayList<Integer>[] adjList = new ArrayList[N+1];
+		for(int i=1; i<=N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
 		
-		List<Integer>[] graph = new ArrayList[N+1];
-		int[] indeg = new int[N+1];
-		for (int i = 1; i<=N; i++) graph[i] = new ArrayList<>();
-		
-		for (int i = 0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] indegree = new int[N+1];
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			graph[a].add(b);
-			indeg[b]++;
-		}
-		
-		ArrayDeque<Integer> q = new ArrayDeque<>();
-		for (int i = 1; i<=N; i++) {
-			if (indeg[i] == 0) q.offer(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			sb.append(cur).append(" ");
 			
-			for (int nxt : graph[cur]) {
-				indeg[nxt]--;
-				if (indeg[nxt] == 0) q.offer(nxt);
+			adjList[a].add(b);
+			indegree[b]++;
+		}
+		
+		Queue<Integer> que = new LinkedList<>();
+		for(int i=1; i<=N; i++) {
+			if(indegree[i]==0) {
+				que.add(i);
 			}
 		}
-		System.out.println(sb);	
+		
+		StringBuilder out = new StringBuilder();
+		
+		while(!que.isEmpty()) {
+			int node = que.poll();
+			out.append(node).append(" ");
+			
+			for(int i : adjList[node]) {
+				indegree[i]--;
+				if(indegree[i]==0) {
+					que.add(i);
+				}
+			}
+		}
+		
+		
+		System.out.println(out.toString());
 	}
 
 }

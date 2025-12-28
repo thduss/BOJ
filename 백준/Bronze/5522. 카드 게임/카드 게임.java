aebaed08f652d2036308248d5cba345e7719648b
Soyeon Kim <109056397+thduss@users.noreply.github.com>
2025-12-28 11:25:43 +0900

    [Bronze V] Title: 카드 게임, Time: 172 ms, Memory: 17672 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5522. 카드 게임/카드 게임.java
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 입력을 받기 위한 Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
-        int sum = 0;
         
+        int total = 0; // 총점을 저장할 변수
+        
+        // 5번 반복하며 숫자를 입력받아 total에 더함
         for (int i = 0; i < 5; i++) {
-            sum += sc.nextInt();
+            total += sc.nextInt();
         }
         
-        System.out.println(sum);
+        // 결과 출력
+        System.out.println(total);
+        
+        // Scanner 닫기 (필수는 아니지만 권장)
         sc.close();
     }
 }

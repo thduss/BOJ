a07a4db305de188a070e479506adb629d2f47886
soyeon <109056397+thduss@users.noreply.github.com>
2026-02-18 13:40:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.57 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -8,44 +8,37 @@ class Solution {
     public int solution(int[][] maps) {
         int answer = 0;
         visited = new boolean[maps.length][maps[0].length];
-        answer = bfs(maps);    
+        
+        answer = bfs(0,0, maps);
         
         return answer;
     }
     
-    
-    public static class Dir{
-        int x, y, len;
+    public static int bfs(int x, int y, int[][] maps){
         
-        public Dir(int x, int y, int len){
-            this.x = x;
-            this.y = y;
-            this.len = len;
-        }
-    }
-    
-    public static int bfs(int[][] maps) {
-        Queue<Dir> que = new LinkedList<>();
-        que.add(new Dir(0,0,1));
-        visited[0][0] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0,1});
         
-        while(!que.isEmpty()){
-            Dir cur = que.poll();
-            
-            if(cur.x==maps.length-1 && cur.y==maps[0].length-1){
-                return cur.len;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            if(cur[0]==maps.length-1 && cur[1]==maps[0].length-1){
+                return cur[2];
             }
             
             for(int i=0; i<4; i++){
-                int nx = cur.x+dx[i];
-                int ny = cur.y+dy[i];
-                
-                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && maps[nx][ny]!= 0 && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    que.add(new Dir(nx,ny,cur.len+1));
+                int nx = dx[i]+cur[0];
+                int ny = dy[i]+cur[1];
+                int cnt=cur[2];
+
+                if(nx>=visited.length || nx<0 || ny>=visited[0].length || ny<0) continue;
+
+                if(maps[nx][ny]==1 && !visited[nx][ny]){
+                    visited[nx][ny]=true;
+                    queue.add(new int[]{nx,ny,cnt+1});
                 }
             }
-        }
+            
+        }   
         
         return -1;
     }

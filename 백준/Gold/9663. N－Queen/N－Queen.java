274d1e22ba60315e4c30ba05a4358a8302900072
soyeon <109056397+thduss@users.noreply.github.com>
2026-02-24 09:31:29 +0900

    [Gold IV] Title: N-Queen, Time: 2564 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,36 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	static int N, ans;
-	static boolean[] col, slash, bSlash;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		col = new boolean[N+1];
-		slash = new boolean[2*N+1];
-		bSlash = new boolean[2*N];
-		
-		ans=0;
-		setQueen(1);
-		System.out.println(ans);
-		
-	}
-	
-	static void setQueen(int row) {
-		if(row > N) {
-			++ans;
-			return;
-		}
-		
-		for(int c=1; c<=N; c++) {
-			
-			if(col[c] || slash[row+c] || bSlash[row-c+N]) continue; // 가지치기
-			// 퀸 배치 내용 자료구조에 기록
-			col[c] = slash[row+c] = bSlash[row-c+N] = true;
-			
-			// 다음 퀸 놓기
-			setQueen(row+1);
-			col[c] = slash[row+c] = bSlash[row-c+N] = false;
-		}
-	}
+    public static boolean[] colCheck, diag1Check, diag2Check;
+    public static int N, count=0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        colCheck = new boolean[N];
+        diag1Check = new boolean[2 * N];
+        diag2Check = new boolean[2 * N];
+
+        dfs(0);
+        System.out.println(count);
+    }
+
+    private static void dfs(int row){
+        if(row == N){
+            count++;
+            return;
+        }
+
+        for(int col =0; col<N; col++){
+            if(isSafe(row, col)){
+                setQueens(row, col, true);
+                dfs(row+1);
+                setQueens(row, col, false);
+            }
+        }
+    }
+
+    private static boolean isSafe(int r, int c){
+        return !colCheck[c] && !diag1Check[r+c] && !diag2Check[r-c+N];
+    }
+
+    private static void setQueens(int r, int c, boolean state){
+        colCheck[c] = state;
+        diag1Check[r+c] = state;
+        diag2Check[r-c+N] = state;
+    }
 }

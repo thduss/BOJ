801975cc4bcf08d3983b7bdf846dc259999b6319
soyeon <109056397+thduss@users.noreply.github.com>
2026-04-28 20:32:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.63 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,40 @@
 import java.util.*;
 
 class Solution {
+    
     public static int[] dx = {0,0,1,-1};
-    public static int[] dy = {1,-1,0,0};
-    public static boolean[][] visited;
+    public static int[] dy = {-1,1,0,0};
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        visited = new boolean[maps.length][maps[0].length];
-        
-        answer = bfs(0,0, maps);
-        
-        return answer;
-    }
-    
-    public static int bfs(int x, int y, int[][] maps){
-        
+        int answer = -1;
+
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0,0,1});
         
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        visited[0][0] = true;
+        
         while(!queue.isEmpty()){
+            
             int[] cur = queue.poll();
             if(cur[0]==maps.length-1 && cur[1]==maps[0].length-1){
-                return cur[2];
+                answer = cur[2];
+                break;
             }
             
             for(int i=0; i<4; i++){
-                int nx = dx[i]+cur[0];
-                int ny = dy[i]+cur[1];
-                int cnt=cur[2];
-
-                if(nx>=visited.length || nx<0 || ny>=visited[0].length || ny<0) continue;
-
-                if(maps[nx][ny]==1 && !visited[nx][ny]){
-                    visited[nx][ny]=true;
-                    queue.add(new int[]{nx,ny,cnt+1});
-                }
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if(nx<0 || ny<0 || nx>=maps.length || ny>=maps[0].length) continue;
+                if(visited[nx][ny] || maps[nx][ny]==0) continue;
+                
+                visited[nx][ny] = true;
+                queue.add(new int[]{nx, ny, cur[2]+1});
             }
             
-        }   
+        }
         
-        return -1;
+        return answer;
     }
 }

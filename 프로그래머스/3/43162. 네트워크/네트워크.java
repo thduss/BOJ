341000aeba2062e00f693f641a2d36f521176fc7
soyeon <109056397+thduss@users.noreply.github.com>
2026-05-08 23:09:10 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,38 +1,32 @@
 import java.util.*;
 
 class Solution {
-    
-    public static boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
-        visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
         
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 answer++;
-                bfs(i, n, computers);
+                queue.add(i);
+                visited[i] = true;
+            }
+            
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+                
+                for(int j=0; j<n; j++){
+                    if(!visited[j] && computers[cur][j]==1){
+                        visited[j] = true;
+                        queue.add(j);
+                    }
+                    
+                }
             }
         }
         
         return answer;
-    }
-    
-    public static void bfs(int s, int n, int[][] computers){
-        
-        Queue<Integer> que = new LinkedList<>();
-        que.add(s);
-        
-        while(!que.isEmpty()){
-            int cur = que.poll();
-            visited[cur] = true;
-            
-            for(int i=0; i<n; i++){
-                if(computers[cur][i]==1 && !visited[i]){
-                    que.add(i);
-                }
-            }
-        }
     }
 }

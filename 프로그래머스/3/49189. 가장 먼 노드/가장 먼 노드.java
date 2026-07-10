9cc8376aa7dc0f0a16dd9f5268940a5fce46f1dd
soyeon <109056397+thduss@users.noreply.github.com>
2026-07-10 18:10:09 +0900

    [level 3] Title: 가장 먼 노드, Time: 18.12 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,46 +1,55 @@
 import java.util.*;
 
 class Solution {
+
+    public static ArrayList<ArrayList<Integer>> graph;
+    
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        graph = new ArrayList<>();
         for(int i=0; i<=n; i++){
-            graph.add(new ArrayList<>());
+            graph.add(new ArrayList<>());   
         }
         
         for(int[] e : edge){
-            int a = e[0];
-            int b = e[1];
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
         
-        int[] dist = new int[n+1];
+        int[] arr = dikstra(1, n);
+        int max = -1;
+        for(int i=1; i<arr.length; i++){
+            if(max<arr[i]){
+                max = arr[i];
+                answer = 0;
+            }
+            
+            if(max==arr[i]) answer++;
+        }
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        dist[1] = 1;
+        return answer;
+    }
+    
+    public static int[] dikstra(int start, int N){
+        int[] arr = new int[N+1];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        arr[1] = 0;
         
-        int maxDist = 0;
+        Queue<Integer> que = new LinkedList<>();
+        que.add(1);
         
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
+        while(!que.isEmpty()){
+            int cur = que.poll();
             
             for(int next : graph.get(cur)){
-                if(dist[next]==0){
-                    dist[next] = dist[cur]+1;
-                    queue.add(next);
-                    maxDist = Math.max(maxDist, dist[next]);
+                if(arr[next]>arr[cur]+1){
+                    arr[next] = arr[cur]+1;
+                    que.add(next);
                 }
             }
         }
         
-        for(int d : dist){
-            if(d==maxDist)
-                answer++;
-        }
-        
-        return answer;
+        return arr;
     }
 }

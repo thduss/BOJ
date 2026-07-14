e381cc8fbef2f27681f3ca2d504f9d8ab202b3bd
soyeon <109056397+thduss@users.noreply.github.com>
2026-07-15 08:57:20 +0900

    [level 3] Title: 섬 연결하기, Time: 0.57 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,54 +1,58 @@
 import java.util.*;
 
 class Solution {
+    
+    public class Node implements Comparable<Node>{
+        int v, c;
+        
+        public Node(int v, int c){
+            this.v = v;
+            this.c = c;
+        }
+        
+        @Override
+        public int compareTo(Node o){
+            return this.c - o.c;
+        }
+    }
+    
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        List<List<int[]>> graph = new ArrayList<>();
-        for(int i=0; i<n; i++){
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
-        for(int[] edge : costs){
-            int u = edge[0];
-            int v = edge[1];
-            int cost = edge[2];
-            
-            graph.get(u).add(new int[]{v, cost});
-            graph.get(v).add(new int[]{u, cost});
+        for(int[] cost : costs){
+            graph.get(cost[0]).add(new Node(cost[1], cost[2]));
+            graph.get(cost[1]).add(new Node(cost[0], cost[2]));
         }
         
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0,0));
         boolean[] visited = new boolean[n];
+        int count = 0;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1]));
-        pq.add(new int[] {0,0});
-        
-        int total = 0, cnt=0;
         while(!pq.isEmpty()){
-            int[] cur = pq.poll();
+            Node cur = pq.poll();
             
-            if(visited[cur[0]]) continue;
+            if(visited[cur.v]) continue;
+            visited[cur.v] = true;
+            count++;
+            answer+=cur.c;
             
-            visited[cur[0]] = true;
-            total+=cur[1];
-            cnt++;
+            if(count==n) break;
             
-            if(cnt==n) break;
-            
-            for(int[] next : graph.get(cur[0])){
-                int nNode = next[0];
-                int nCost = next[1];
+            for(Node next : graph.get(cur.v)){
+                if(visited[next.v]) continue;
                 
-                if(!visited[nNode]){
-                    pq.add(new int[]{nNode, nCost});
-                }
+                pq.add(new Node(next.v, next.c));
             }
             
         }
         
-        answer = total;
+        
         return answer;
     }
-    
-    
 }

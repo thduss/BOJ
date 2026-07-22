de5a1a83a4813159233042f94b11b3328fa39940
soyeon <109056397+thduss@users.noreply.github.com>
2026-07-22 09:28:00 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 80.41 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -3,38 +3,41 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = {};
-
-        Map<String, Integer> money = new HashMap<>();
-            
-        Map<String, String> parents = new HashMap<>();
+        
+        Map<String, String> parent = new HashMap<>();
         for(int i=0; i<enroll.length; i++){
-            parents.put(enroll[i], referral[i]);
+            parent.put(enroll[i], referral[i]);
         }
         
+        // 10% 부모에게 배분 + 90% 본인
+        Map<String, Integer> sell = new HashMap<>();
+        
         for(int i=0; i<seller.length; i++){
+            Queue<String> queue = new LinkedList<>();
+            queue.add(seller[i]);
+            int money = amount[i]*100;
             
-            String person = seller[i];
-            int currentMoney = amount[i] * 100;
-            
-            while(!person.equals("-")){
-                // 부모에게 줄 돈
-                int give = currentMoney/10;
+            while(!queue.isEmpty()){
+                String cur = queue.poll();
                 
-                int mine = currentMoney - give;
-                money.put(person, money.getOrDefault(person, 0)+mine);
+                if(cur.equals("-") || money == 0){
+                    break;
+                }
                 
-                if(give==0) break;
+                int give = money / 10;
+                int mine = money - give;
                 
-                person = parents.get(person);
-                currentMoney = give;
+                sell.put(cur, sell.getOrDefault(cur, 0)+mine);
+                money = give;
+                
+                queue.add(parent.get(cur));
             }
         }
         
         answer = new int[enroll.length];
         for(int i=0; i<enroll.length; i++){
-            answer[i] = money.getOrDefault(enroll[i],0);
+            answer[i] = sell.getOrDefault(enroll[i],0);
         }
-        
         
         return answer;
     }

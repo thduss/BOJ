33592a0a5e13b17f7e16d03efb43c33bc61b6c63
soyeon <109056397+thduss@users.noreply.github.com>
2026-07-22 09:57:07 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,13 +1,17 @@
-SELECT A.EMP_NO, A.EMP_NAME, CASE WHEN AVG(SCORE)>=96 THEN 'S'
-                                  WHEN AVG(SCORE)>=90 THEN 'A'
-                                  WHEN AVG(SCORE)>=80 THEN 'B'
-                                  ELSE 'C' END AS GRADE,
-                             CASE WHEN AVG(SCORE)>=96 THEN 0.2*A.SAL
-                                  WHEN AVG(SCORE)>=90 THEN 0.15*A.SAL
-                                  WHEN AVG(SCORE)>=80 THEN 0.1*A.SAL
-                                  ELSE 0 END AS BONUS
-FROM HR_EMPLOYEES A
-LEFT JOIN HR_DEPARTMENT B ON A.DEPT_ID=B.DEPT_ID
-JOIN HR_GRADE C ON A.EMP_NO=C.EMP_NO
-GROUP BY A.EMP_NO, A.EMP_NAME
-ORDER BY EMP_NO;
+# 사원별 성과금 정보를 조회
+# 사번, 성명, 평가 등급, 성과금을 조회
+# 평가등급: GRADE, 성과금: BONUS
+# 사번 기준으로 오름차순 정렬
+
+SELECT A.EMP_NO, B.EMP_NAME, CASE WHEN AVG(A.SCORE) >= 96 THEN 'S'
+                                  WHEN AVG(A.SCORE) >= 90 THEN 'A'
+                                  WHEN AVG(A.SCORE) >= 80 THEN 'B'
+                                  ELSE 'C' END AS GRADE, 
+                            CASE WHEN AVG(A.SCORE) >= 96 THEN B.SAL*0.2
+                                 WHEN AVG(A.SCORE) >= 90 THEN B.SAL*0.15
+                                 WHEN AVG(A.SCORE) >= 80 THEN B.SAL*0.10
+                                 ELSE 0 END AS BONUS
+FROM HR_GRADE A
+LEFT JOIN HR_EMPLOYEES B ON A.EMP_NO = B.EMP_NO
+GROUP BY A.EMP_NO, B.EMP_NAME
+ORDER BY EMP_NO ASC;

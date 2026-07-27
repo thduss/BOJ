4ac528f19fcc8d83cbb0250c66a2afd7d447e5a4
soyeon <109056397+thduss@users.noreply.github.com>
2026-07-27 11:02:48 +0900

    [level 3] Title: 섬 연결하기, Time: 1.74 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,57 +1,46 @@
 import java.util.*;
 
+// 크루스칼 알고리즘 사용해서 MST 찾기
 class Solution {
+    public static int[] parent;
     
-    public class Node implements Comparable<Node>{
-        int v, c;
+    // 부모 찾기
+    public int find(int a){
+        if(parent[a]==a) return a;
         
-        public Node(int v, int c){
-            this.v = v;
-            this.c = c;
-        }
+        return parent[a] = find(parent[a]);
+    }
+    
+     // a와 b 같은 그룹?
+    public boolean union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot == bRoot) return false; // 사이클 발생
         
-        @Override
-        public int compareTo(Node o){
-            return this.c - o.c;
-        }
+        if(aRoot>bRoot) parent[bRoot] = aRoot;
+        else parent[aRoot] = bRoot;
+        
+        return true;
     }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        for(int i=0; i<=n; i++){
-            graph.add(new ArrayList<>());
-        }
+        parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
         
-        for(int[] cost : costs){
-            graph.get(cost[0]).add(new Node(cost[1], cost[2]));
-            graph.get(cost[1]).add(new Node(cost[0], cost[2]));
-        }
+        // cost 기준으로 오름차순 정렬
+        Arrays.sort(costs, (a,b) -> Integer.compare(a[2], b[2]));
         
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(0,0));
-        boolean[] visited = new boolean[n];
-        int count = 0;
-        
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
+        for(int[] e : costs){
+            int a = e[0];
+            int b = e[1];
+            int w = e[2];
             
-            if(visited[cur.v]) continue;
-            visited[cur.v] = true;
-            count++;
-            answer+=cur.c;
-            
-            if(count==n) break;
-            
-            for(Node next : graph.get(cur.v)){
-                if(visited[next.v]) continue;
-                
-                pq.add(new Node(next.v, next.c));
+            if(union(a,b)){
+                answer+=w;
             }
-            
         }
-        
         
         return answer;
     }

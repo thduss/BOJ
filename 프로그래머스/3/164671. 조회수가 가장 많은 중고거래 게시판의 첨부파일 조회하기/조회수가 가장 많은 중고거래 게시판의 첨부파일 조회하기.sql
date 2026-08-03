0c133218042f8dd15921c49b6974a7eb347851c6
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-03 09:18:48 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,10 @@
+# 조회수가 가장 높은 중고거래 게시물 첨부파일 경로 조회
+# FILE ID 내림차순
+# 기본 경로 /home/grep/src/
+
+SELECT CONCAT('/home/grep/src/', A.BOARD_ID, '/', A.FILE_ID, A.FILE_NAME, A.FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE A
+LEFT JOIN USED_GOODS_BOARD B ON A.BOARD_ID = B.BOARD_ID
+WHERE B.VIEWS = (SELECT MAX(VIEWS)
+                  FROM USED_GOODS_BOARD)
+ORDER BY FILE_ID DESC;

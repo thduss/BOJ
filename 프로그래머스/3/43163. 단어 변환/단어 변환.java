f98de030146ac151ee7d773f7f9af8da0c9071a4
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-06 10:14:30 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,49 +2,47 @@ import java.util.*;
 
 class Solution {
     public class Node{
+        String word;
         int cnt;
-        String str;
         
-        public Node(int cnt, String str){
+        public Node(String word, int cnt){
+            this.word = word;
             this.cnt = cnt;
-            this.str = str;
         }
     }
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-
-        Queue<Node> queue = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
-        queue.add(new Node(0, begin));
         
+        boolean[] visited = new boolean[words.length];
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(begin, 0));
         
         while(!queue.isEmpty()){
             Node cur = queue.poll();
             
-            if(cur.str.equals(target)){
+            if(cur.word.equals(target)){
                 answer = cur.cnt;
                 break;
             }
             
             for(int i=0; i<words.length; i++){
-                if(visited[i]) continue;
-                
-                if(check(cur.str, words[i])){
+                if(!visited[i] && check(words[i], cur.word)){
                     visited[i] = true;
-                    queue.add(new Node(cur.cnt+1, words[i]));
+                    queue.add(new Node(words[i], cur.cnt+1));
                 }
             }
+            
         }
         
         return answer;
     }
     
-    public boolean check(String s1, String s2){
-        int diff=0;
+    public boolean check(String str1, String str2){
+        int diff = 0;
         
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i)!=s2.charAt(i)) diff++;
+        for(int i=0; i<str1.length(); i++){
+            if(str1.charAt(i)!=str2.charAt(i)) diff++;
         }
         
         return diff==1 ? true : false;

ea7dfefa8d49db6e8ddf8bd8ba8ec67b84d716a4
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-26 11:49:12 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.12 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,51 +1,52 @@
 import java.util.*;
 
 class Solution {
-    public static List<List<Integer>> list;
+    List<List<Integer>> graph;
     
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
 
-        list = new ArrayList<>();
-        for(int i=0; i<=n; i++){
-            list.add(new ArrayList<>());
-        }
+        graph = new ArrayList<>();
+        for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
         
-        for(int[] wire : wires){
-            list.get(wire[0]).add(wire[1]);
-            list.get(wire[1]).add(wire[0]);
+        for(int[] w : wires){
+            graph.get(w[0]).add(w[1]);
+            graph.get(w[1]).add(w[0]);
         }
         
         for(int[] w : wires){
-            list.get(w[0]).remove(Integer.valueOf(w[1]));
-            list.get(w[1]).remove(Integer.valueOf(w[0]));
+            graph.get(w[0]).remove(Integer.valueOf(w[1]));
+            graph.get(w[1]).remove(Integer.valueOf(w[0]));
             
-            int num = find(1);                
-            answer = Math.min(answer, Math.abs(num-(n-num)));
+            int a = find(w[0], n);
+            int b = find(w[1], n);
             
-            list.get(w[0]).add(w[1]);
-            list.get(w[1]).add(w[0]);
+            answer = Math.min(answer, Math.abs(a-b));
+            
+            graph.get(w[0]).add(w[1]);
+            graph.get(w[1]).add(w[0]);
         }
         
         return answer;
     }
     
-    public int find(int start){
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[list.size()];
-        
-        visited[start] = true;
-        queue.add(start);
+    public int find(int start, int N){
         int cnt=0;
+        
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        
+        boolean[] visited = new boolean[N+1];
+        visited[start] = true;
         
         while(!queue.isEmpty()){
             int cur = queue.poll();
             cnt++;
             
-            for(int i : list.get(cur)){
-                if(!visited[i]){
-                    visited[i] = true;
-                    queue.add(i);
+            for(int nxt : graph.get(cur)){
+                if(!visited[nxt]){
+                    visited[nxt] = true;
+                    queue.add(nxt);
                 }
             }
         }

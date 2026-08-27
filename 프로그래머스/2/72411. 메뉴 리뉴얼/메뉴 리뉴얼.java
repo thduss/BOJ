c2a05602b59c2c6d9c31da64e128490bf3b2bbb2
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-27 09:36:49 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 16.72 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,50 +1,67 @@
 import java.util.*;
 
-class Solution { 
-    public static Map<String, Integer> map;
-    
+class Solution {
     public String[] solution(String[] orders, int[] course) {
-        List<String> answerList = new ArrayList<>();
-   
+        String[] answer = {};
+        ans = new ArrayList<>();
+        
+        for(int i=0; i<orders.length; i++){
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
+            orders[i] = str;
+        }
         
         for(int i=0; i<course.length; i++){
-            map = new HashMap<>(); 
-            
-            for(String order : orders){
-                char[] charArray = order.toCharArray();
-                Arrays.sort(charArray);
-                String sortedOrder = new String(charArray);
-                
-                if (sortedOrder.length() >= course[i]) {
-                    comb(0, sortedOrder, new StringBuilder(), course[i]);
-                }
-            }
-            
-            if (!map.isEmpty()) {
-                int max = Collections.max(map.values());
-                if (max >= 2) {
-                    for (String key : map.keySet()) {
-                        if (map.get(key) == max) answerList.add(key);
-                    }
-                }
-            }
+            find(orders, course[i]);
         }
-    
-        Collections.sort(answerList);
-        return answerList.toArray(new String[0]);
+        
+        answer = new String[ans.size()];
+        Collections.sort(ans);
+        for(int i=0; i<ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+        return answer;
     }
     
-    public void comb(int start, String order, StringBuilder sb, int r) {
-        if (r == 0) {
-            String res = sb.toString();
-            map.put(res, map.getOrDefault(res, 0) + 1);
+    public Map<String, Integer> map;
+    List<String> ans;
+    
+    public void find(String[] orders, int len){
+        map = new HashMap<>();
+        
+        for(String order : orders){
+            comb(0, "", order, len);
+        }
+        
+        int max = 0;
+        List<String> maxList = new ArrayList<>();
+        for(String key : map.keySet()){
+            if(max<map.get(key)){
+                max = map.get(key);
+                maxList = new ArrayList<>();
+                maxList.add(key);
+            } else if (max==map.get(key)){
+                maxList.add(key);
+            }
+        }
+        
+        if(max<2) return;
+        for(String str : maxList){
+            ans.add(str);
+        }
+    }
+    
+    public void comb(int idx, String str, String word, int len){
+        if(str.length()==len){
+            map.put(str, map.getOrDefault(str, 0)+1);
             return;
         }
         
-        for (int i = start; i < order.length(); i++) {
-            sb.append(order.charAt(i));
-            comb(i + 1, order, sb, r - 1);
-            sb.setLength(sb.length() - 1);
-        }
+        if(str.length()>=len) return;
+        if(idx>=word.length()) return;
+        
+        comb(idx+1, str+word.charAt(idx), word, len);  
+        comb(idx+1, str, word, len);
     }
 }

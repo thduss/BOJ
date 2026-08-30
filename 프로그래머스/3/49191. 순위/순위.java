70ab54d92c2b4d7527fa988632423ca6c5a2ec53
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-30 20:39:13 +0900

    [level 3] Title: 순위, Time: 24.98 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,56 +1,50 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] results) {
         int answer = 0;
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> reverse = new ArrayList<>();
-        
-        for(int i=0; i<=n; i++){
-            graph.add(new ArrayList<>());
-            reverse.add(new ArrayList<>());
+        List<List<Integer>> win = new ArrayList<>();
+        List<List<Integer>> lose = new ArrayList<>();
+        for(int i=0; i<=n; i++) {
+            win.add(new ArrayList<>());
+            lose.add(new ArrayList<>());
         }
         
         for(int[] r : results){
-            graph.get(r[0]).add(r[1]);
-            reverse.get(r[1]).add(r[0]);
+            win.get(r[0]).add(r[1]);
+            lose.get(r[1]).add(r[0]);
         }
         
         for(int i=1; i<=n; i++){
-            int pre = count(i, n, graph);
-            int post = count(i, n, reverse);
+            int wcnt = find(i, n, win);
+            int lcnt = find(i, n, lose);
             
-            if(pre+post+1 == n){
-                answer++;
-            }
+            if(wcnt+lcnt==n-1) answer++;
         }
         
         return answer;
     }
     
-    public int count(int i, int n, ArrayList<ArrayList<Integer>> graph){
-        int count = 0;
-        boolean[] visited = new boolean[n+1];
+    public int find(int node, int N, List<List<Integer>> graph){
+        int cnt=0;
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(i);
-        visited[i] = true;
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[N+1];
+        queue.add(node);
+        visited[node] = true;
         
         while(!queue.isEmpty()){
             int cur = queue.poll();
-            count++;
             
-            for(int next : graph.get(cur)){
-                if(!visited[next]){
-                    visited[next] = true;
-                    queue.add(next);
-                }
+            if(!visited[cur]) cnt++;
+            visited[cur] = true;
+            
+            for(int nxt : graph.get(cur)){
+                if(!visited[nxt]) queue.add(nxt);
             }
         }
         
-        return count-1;
-        
+        return cnt;
     }
 }

fb8da83b5f329ce966914aea5f74ee70e581b12b
soyeon <109056397+thduss@users.noreply.github.com>
2026-08-30 17:19:34 +0900

    [level 2] Title: 요격 시스템, Time: 243.78 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        int answer = 0;
+        int answer = 1;
         
-        // 종료 시점 기준 오름차순 정렬
-        Arrays.sort(targets, (o1, o2) -> {
-            return o1[1] - o2[1];
-        });
+        Arrays.sort(targets, (a, b) -> b[1] - a[1]);
         
-        int lastShoot = -1;
-        
-        for(int[] target : targets){
-            int start = target[0];
-            int end = target[1];
-            
-            if(lastShoot <= start){
-                lastShoot = end;
+        int s = targets[0][0], e = targets[0][1];
+        for(int[] t : targets){
+            if(t[1]<=s){
+                s = t[0];
+                e = t[1];
                 answer++;
+                continue;
             }
+            
+            s = Math.max(s, t[0]);
+            e = Math.min(e, t[1]);
         }
         
         return answer;

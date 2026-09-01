9ef33499ac84c259a6fcaffca39780db29d4768d
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-01 09:00:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.22 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,52 +1,64 @@
+/*
+- 작업 번호, 요청 시각, 소요 시간
+- 소요시간 짧 > 요청 시각 빠름 > 작업 번호 작음
+- 반환 시간 = 끝남 - 요청
+*/
 import java.util.*;
 
 class Solution {
-    public class Node implements Comparable<Node>{
-        int t1, t2, t3;
+    public class Node implements Comparable<Node> {
+        int no, requestTime, serviceTime;
         
-        public Node(int t1, int t2, int t3){
-            this.t1 = t1;
-            this.t2 = t2;
-            this.t3 = t3;
+        public Node(int no, int requestTime, int serviceTime){
+            this.no = no;
+            this.requestTime = requestTime;
+            this.serviceTime = serviceTime;
         }
         
-        // 큐 순서 : 소요시간 짧은것 > 요청시각 빠른것 > 작업 번호 작은것
         @Override
         public int compareTo(Node o){
-            if(t1==o.t1){
-                if(t2==o.t2){
-                    return t3 - o.t3;
-                }
-                return t2 - o.t2;
+            if(this.serviceTime != o.serviceTime){
+                return this.serviceTime - o.serviceTime;
             }
-            return t1 - o.t1;
+            
+            if(this.requestTime != o.requestTime){
+                return this.requestTime - o.requestTime;
+            }
+            
+            return this.no - o.no;
         }
     }
+    
     public int solution(int[][] jobs) {
         int answer = 0;
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        int time=0;
-        int idx=0;
+        Arrays.sort(jobs, (a,b)-> a[0] - b[0]);
         
-        Arrays.sort(jobs, (a,b)-> a[0]-b[0]);
-        
-        while(idx<jobs.length || !pq.isEmpty()){
-            
+        int time = 0, idx=0;
+        while(idx<jobs.length){
             while(idx<jobs.length && jobs[idx][0]<=time){
-                pq.add(new Node(jobs[idx][1], jobs[idx][0], idx));
+                pq.add(new Node(idx, jobs[idx][0], jobs[idx][1]));
                 idx++;
             }
             
             if(!pq.isEmpty()){
                 Node cur = pq.poll();
-                time += cur.t1;
-                answer += (time-cur.t2);
+                time += cur.serviceTime;
+                answer += (time - cur.requestTime);
             } else {
                 time = jobs[idx][0];
             }
         }
-            
-        return answer/jobs.length;
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            time += cur.serviceTime;
+            answer += (time - cur.requestTime);
+        }
+        
+        answer /= jobs.length;
+        
+        return answer;
     }
 }

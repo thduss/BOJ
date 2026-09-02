f11f9ca36cac92a1d676b2be79169d801a70c1a9
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-02 11:23:03 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1887.04 ms, Memory: 387 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,69 +1,56 @@
 import java.util.*;
 
 class Solution {
-    public static class Node implements Comparable<Node> {
+    public class Node implements Comparable<Node> {
         int x, y;
-        StringBuilder sb;
+        String str;
         
-        public Node(int x, int y, StringBuilder sb){
+        Node(int x, int y, String str){
             this.x = x;
             this.y = y;
-            this.sb = sb;
+            this.str = str;
         }
         
         @Override
         public int compareTo(Node o){
-            return this.sb.toString().compareTo(o.sb.toString());
+            return this.str.compareTo(o.str);
         }
     }
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "impossible";
-
-        int minDist = Math.abs(x-r) + Math.abs(y-c);
-        if(minDist>k || (k-minDist)%2!=0) {
-            return "impossible";
-        }
+        String answer = "";
         
         int[] dx = {0,0,-1,1};
         int[] dy = {-1,1,0,0};
         String[] cmd = {"l", "r", "u", "d"};
-        
+
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(x,y,new StringBuilder()));
+        pq.add(new Node(x, y, ""));
         boolean[][][] visited = new boolean[n+1][m+1][k+1];
         
         while(!pq.isEmpty()){
             Node cur = pq.poll();
-            int len = cur.sb.length();
-            visited[cur.x][cur.y][len] = true;
-            
-            if(len==k && cur.x==r && cur.y==c){
-                answer = cur.sb.toString();
+            visited[cur.x][cur.y][cur.str.length()] = true;
+                            
+            if(cur.x==r && cur.y==c && cur.str.length()==k){
+                answer = cur.str;
                 break;
             }
             
-            if(len>=k) continue;
+            if(cur.str.length()>=k) continue;
             
             for(int i=0; i<4; i++){
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
                 
-                if(nx<=0 || nx>n || ny<=0 || ny>m){
-                    continue;
-                }
+                if(nx<=0 || nx>n || ny<=0 || ny>m) continue;
                 
-                if(visited[nx][ny][len+1]) continue;
-                
-                StringBuilder sb = new StringBuilder(cur.sb).append(cmd[i]);
-                
-                pq.add(new Node(nx, ny, sb));
+                if(visited[nx][ny][cur.str.length()+1]) continue;
+                pq.add(new Node(nx, ny, cur.str + cmd[i]));
             }
-            
         }
         
+        answer = answer.equals("") ? "impossible" : answer;
         return answer;
     }
-    
-    
 }

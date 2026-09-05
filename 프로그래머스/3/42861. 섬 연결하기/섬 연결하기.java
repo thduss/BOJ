fe2f8497bbde4a98c0da9ebae9384e15602bc33a
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-05 10:44:45 +0900

    [level 3] Title: 섬 연결하기, Time: 0.89 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,44 +1,43 @@
 import java.util.*;
 
-// 크루스칼 알고리즘 사용해서 MST 찾기
 class Solution {
-    public static int[] parent;
-    
-    // 부모 찾기
-    public int find(int a){
-        if(parent[a]==a) return a;
-        
-        return parent[a] = find(parent[a]);
-    }
-    
-     // a와 b 같은 그룹?
-    public boolean union(int a, int b){
-        int aRoot = find(a);
-        int bRoot = find(b);
-        if(aRoot == bRoot) return false; // 사이클 발생
-        
-        if(aRoot>bRoot) parent[bRoot] = aRoot;
-        else parent[aRoot] = bRoot;
-        
-        return true;
-    }
+    int[] parent;
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        
+
         parent = new int[n];
         for(int i=0; i<n; i++) parent[i] = i;
         
-        // cost 기준으로 오름차순 정렬
-        Arrays.sort(costs, (a,b) -> Integer.compare(a[2], b[2]));
+        answer = kruskal(costs);
+        
+        return answer;
+    }
+    
+    public void union(int x, int y){
+        x = find(x);
+        y = find(y);
+        
+        if(x!=y) parent[x] = y; 
+    }
+    
+    public int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    public int kruskal(int[][] costs){
+        Arrays.sort(costs, (a,b) -> a[2]-b[2]);
+        int answer=0;
         
         for(int[] e : costs){
-            int a = e[0];
-            int b = e[1];
-            int w = e[2];
+            int u = e[0];
+            int v = e[1];
+            int c = e[2];
             
-            if(union(a,b)){
-                answer+=w;
+            if(find(u)!=find(v)) {
+                union(u,v);
+                answer += c;
             }
         }
         

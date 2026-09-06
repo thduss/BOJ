fe89e7e05c46a2e0fe79a7de82f4c5d86f0df13c
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-06 09:11:38 +0900

    [level 3] Title: 섬 연결하기, Time: 0.79 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,46 +1,34 @@
 import java.util.*;
 
 class Solution {
-    int[] parent;
+    public int[] parent;
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-
         parent = new int[n];
         for(int i=0; i<n; i++) parent[i] = i;
+        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         
-        answer = kruskal(costs);
-        
-        return answer;
-    }
-    
-    public void union(int x, int y){
-        x = find(x);
-        y = find(y);
-        
-        if(x!=y) parent[x] = y; 
-    }
-    
-    public int find(int x){
-        if(parent[x]==x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    public int kruskal(int[][] costs){
-        Arrays.sort(costs, (a,b) -> a[2]-b[2]);
-        int answer=0;
-        
-        for(int[] e : costs){
-            int u = e[0];
-            int v = e[1];
-            int c = e[2];
-            
-            if(find(u)!=find(v)) {
-                union(u,v);
-                answer += c;
+        for(int[] c : costs){
+            if(find(c[0])!=find(c[1])){
+                union(c[0], c[1]);
+                answer += c[2];
             }
         }
         
         return answer;
+    }
+    
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        
+        if(a!=b) parent[a] = b;
+    }
+    
+    public int find(int a){
+        if(parent[a]==a) return a;
+        
+        return parent[a] = find(parent[a]);
     }
 }

ba513787fbd0cf0eaea4e0ed5a3e8554e5d8129d
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-09 10:15:43 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 24.24 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,35 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
-        
-        Set<String> g_size = new HashSet<>();
-        for(String g : gems) g_size.add(g);
-        int size = g_size.size();
+        int[] answer = new int[2];
 
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        for(String str : gems){
+            set.add(str);
+        }
+        int cnt = set.size();
         
-        int s = 0;
-        int minLen = gems.length;
-        answer = new int[2];
+        Map<String, Integer> map = new HashMap<>();
+        int s = 0, minLen=gems.length;
         for(int e=0; e<gems.length; e++){
-            map.put(gems[e], map.getOrDefault(gems[e],0)+1);
+            map.put(gems[e], map.getOrDefault(gems[e], 0)+1);
             
-            // start 위치 조정
-            while(map.size()==size){
-                if(e-s<minLen){
-                    minLen = e-s;
+            while(map.size() == cnt){
+                if(e - s < minLen){
+                    minLen = e - s;
                     answer[0] = s+1;
                     answer[1] = e+1;
                 }
                 
-                map.put(gems[s], map.get(gems[s])-1); 
-                if(map.get(gems[s])==0){
-                    map.remove(gems[s]);
-                }
+                map.put(gems[s], map.get(gems[s])-1);
+                if(map.get(gems[s])==0) map.remove(gems[s]);
                 s++;
             }
-            
         }
         
         return answer;

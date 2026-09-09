0bee9e0f4544f164439bfed051600c64c31da33f
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-10 08:56:26 +0900

    [level 2] Title: 과제 진행하기, Time: 6.50 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,114 +1,68 @@
 import java.util.*;
 
 class Solution {
-    public class Task implements Comparable<Task> {
-        String subject;
-        int h, m, time;
+    public class Node{
+        int time;
+        String name;
         
-        public Task(String subject, int h, int m, int time){
-            this.subject = subject;
-            this.h = h;
-            this.m = m;
+        Node(int time, String name){
             this.time = time;
-        } 
-        
-        @Override
-        public int compareTo(Task o){
-            if(this.h==o.h){
-                return this.m - o.m;
-            } else {
-                return this.h - o.h;
-            }
+            this.name = name;
         }
     }
     
     public String[] solution(String[][] plans) {
-        String[] answer = {};
-
-        List<Task> list = new ArrayList<>();
-        
-        for(String[] plan : plans){
-            String[] tmp = plan[1].split(":");
-            int h = Integer.parseInt(tmp[0]);
-            int m = Integer.parseInt(tmp[1]);
-            
-            list.add(new Task(plan[0], h, m, Integer.parseInt(plan[2])));
-        }
-        
-        Collections.sort(list);
-        
+        String[] answer = new String[plans.length];
         List<String> ans = new ArrayList<>();
-        int hour=list.get(0).h, min=list.get(0).m;
-        Stack<Task> st = new Stack<>();
         
-        for(int i=0; i<list.size(); i++){
-            Task cur = list.get(i);
-            min += cur.time;
-            if(min>=60){
-                hour+=(min/60);
-                min%=60;
-            }
-            
-            if(i==list.size()-1){
-                ans.add(cur.subject);
+        Arrays.sort(plans, (a,b) -> a[1].compareTo(b[1]));
+        
+        Stack<Node> st = new Stack<>();        
+        for(int i=0; i<plans.length; i++){
+            if(i==plans.length-1) {
+                ans.add(plans[i][0]);
                 continue;
             }
+            String[] tmp = plans[i][1].split(":");
+            int h = Integer.parseInt(tmp[0]);
+            int m = Integer.parseInt(tmp[1]);
+            int play = Integer.parseInt(plans[i][2]);
             
-            // 다음 과제 이전에 끝나?
-            Task next = list.get(i+1);
+            tmp = plans[i+1][1].split(":");
+            int nh = Integer.parseInt(tmp[0]);
+            int nm = Integer.parseInt(tmp[1]);
             
-            if(next.h>hour || (next.h==hour && next.m>=min)){
-                ans.add(cur.subject);
+            int time = (nh*60+nm) - (h*60+m+play);
+            
+            if(time>=0){
+                ans.add(plans[i][0]);
                 
-                // 대기 중인 과제는 다음 과제 이전에 끝나?
-                while(!st.isEmpty()){
-                    Task ref = st.peek();
-                    int rm = min + ref.time;
-                    int rh = hour;
-                    if(rm>=60){
-                        rh+=(rm/60);
-                        rm%=60;
-                    }
+                while(time>0 && !st.isEmpty()){
+                    Node cur = st.pop();
                     
-                    if(next.h>rh || (next.h==rh && next.m>=rm)){
-                        hour = rh;
-                        min = rm;
-                        
-                        st.pop();
-                        ans.add(ref.subject);
+                    if(cur.time>time){
+                        st.add(new Node(cur.time-time, cur.name));
+                        time = 0;
                     } else {
-                        int diff = (next.h - hour) * 60 + next.m - min;
-                        Task r = st.pop();
-                        r.time -= diff;
-                        st.add(r);
-                        hour = next.h;
-                        min = next.m;
-                        
-                        break;
+                        time-=cur.time;
+                        ans.add(cur.name);
                     }
-                }
-                
-                if(st.isEmpty()){
-                    hour = next.h;
-                    min = next.m;
                 }
                 
             } else {
-                int diff = (next.h - cur.h) * 60 + next.m - cur.m;
-                cur.time -= diff;
-                st.add(cur);
-                hour = next.h;
-                min = next.m;
+                st.add(new Node(-time, plans[i][0]));
             }
-            
         }
         
-        // 남은 과제 하나씩 해결
         while(!st.isEmpty()){
-            Task cur = st.pop();
-            ans.add(cur.subject);
+            Node cur = st.pop();
+            ans.add(cur.name);
         }
         
-        return ans.toArray(new String[ans.size()]);
+        for(int i=0; i<ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
+        
+        return answer;
     }
 }

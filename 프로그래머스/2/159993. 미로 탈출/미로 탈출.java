5c90e5a72191dedaf13fa5a649b6915bbee00642
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-11 21:23:38 +0900

    [level 2] Title: 미로 탈출, Time: 5.69 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -3,65 +3,69 @@ import java.util.*;
 class Solution {
     public int solution(String[] maps) {
         int answer = 0;
-
-        // 위치 찾기
-        int lx = 0, ly = 0;
-        int sx = 0, sy = 0;
-        int ex = 0, ey = 0;
         
-        for(int i=0; i<maps.length; i++){
-            for(int j=0; j<maps[0].length(); j++){
-                if(maps[i].charAt(j)=='L'){
-                    lx = i;
-                    ly = j;
-                } else if(maps[i].charAt(j)=='E'){
-                    ex = i;
-                    ey = j;
-                } else if(maps[i].charAt(j)=='S'){
+        int sx=0, sy=0, ex=0, ey=0, lx=0, ly=0;
+        int N = maps.length;
+        int M = maps[0].length();
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(maps[i].charAt(j)=='S') {
                     sx = i;
                     sy = j;
-                }
+                }  
+                
+                if(maps[i].charAt(j)=='E') {
+                    ex = i;
+                    ey = j;
+                }  
+                
+                if(maps[i].charAt(j)=='L') {
+                    lx = i;
+                    ly = j;
+                }  
             }
         }
         
-        int labber = bfs(sx, sy, lx, ly, maps);
-        int exit = bfs(lx, ly, ex, ey, maps);
-        // System.out.println(labber + " " + exit);
-        if(labber==-1 || exit==-1) return -1;
+        int d1 = bfs(sx, sy, lx, ly, N, M, maps);
+        int d2 = bfs(lx, ly, ex, ey, N, M, maps);
         
-        return labber + exit;
+        if(d1==-1 || d2==-1) return -1;
+        answer = d1 + d2;
+        
+        return answer;
     }
     
-    public static int[] dx = {0,0,-1,1};
-    public static int[] dy = {-1,1,0,0};
-    
-    public static int bfs(int sx, int sy, int ex, int ey, String[] maps){
-        int N = maps.length, M = maps[0].length();
-        int answer = -1;
+    public int bfs(int sx, int sy, int ex, int ey, int N, int M, String[] maps){
+        int[] dx = {0,0,-1,1};
+        int[] dy = {-1,1,0,0};
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{sx, sy, 0});
         
         boolean[][] visited = new boolean[N][M];
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{sx, sy, 0});
+        visited[sx][sy] = true;
         
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
             
             if(cur[0]==ex && cur[1]==ey){
-                answer = cur[2];
-                break;
+                return cur[2];
             }
             
             for(int i=0; i<4; i++){
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                if(nx<0 || nx>=N || ny<0 || ny>=M || visited[nx][ny] || maps[nx].charAt(ny)=='X') continue;
+                if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
+                if(visited[nx][ny]) continue;
+                if(maps[nx].charAt(ny)=='X') continue;
+                
                 visited[nx][ny] = true;
                 queue.add(new int[]{nx, ny, cur[2]+1});
             }
         }
         
-        return answer;
+        return -1;
     }
 }

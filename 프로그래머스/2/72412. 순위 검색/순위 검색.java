4b946c32f463e1dd6186f5ff8674b61a7173232e
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-11 18:51:39 +0900

    [level 2] Title: 순위 검색, Time: 723.05 ms, Memory: 407 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,68 +1,73 @@
 import java.util.*;
 
 class Solution {
-
-    Map<String, ArrayList<Integer>> map;
+    public static Map<String, List<Integer>> people;
     
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
+        people = new HashMap<>();
         
-        // 조합
-        map = new HashMap<>();
-        for(String str : info){
-            String[] p = str.split(" ");
-            make_comb(p, "", 0);
+        for(int i=0; i<info.length; i++){
+            String[] arr = info[i].split(" ");
+            
+            dfs(0, "", arr);
         }
         
-        for(String key : map.keySet()){
-            Collections.sort(map.get(key));
+        for(String key : people.keySet()){
+            Collections.sort(people.get(key));
         }
         
         for(int i=0; i<query.length; i++){
-            query[i] = query[i].replace("and ", "");
-            String[] q = query[i].split(" ");
+            String q = query[i];
+            String[] arr = q.split(" ");
+            int score = Integer.parseInt(arr[7]);
             
-            String key = q[0] + q[1] + q[2] + q[3];
-            int score = Integer.parseInt(q[4]);
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[j].equals("and")) continue;
+                sb.append(arr[j]);
+            }
             
-            if(map.containsKey(key)){
-                ArrayList<Integer> scores = map.get(key);
-                
-                // 이분탐색
-                answer[i] = binarySearch(scores, score);
-            } else {
+            if(!people.containsKey(sb.toString())) {
                 answer[i] = 0;
+            } else {
+                answer[i] = binarySearch(people.get(sb.toString()), score);
             }
         }
         
         return answer;
     }
     
-    public void make_comb(String[] info, String str, int depth){
-        if(depth==4){
-            int score = Integer.parseInt(info[4]);
-            map.putIfAbsent(str, new ArrayList<>());
-            map.get(str).add(score);
+    public static void dfs(int idx, String str, String[] arr){
+        if(idx>=arr.length){
+            people.putIfAbsent(str.split(" ")[0], new ArrayList<>());
+            int score = Integer.parseInt(str.split(" ")[1]);
+            people.get(str.split(" ")[0]).add(score);
             return;
         }
         
-        make_comb(info, str + info[depth], depth+1);
-        make_comb(info, str + "-", depth+1);
+        if(idx==arr.length-1) {
+            dfs(idx+1, str + " " + arr[idx], arr);
+            return;
+        }
+        
+        dfs(idx+1, str + "-", arr);
+        dfs(idx+1, str + arr[idx], arr);
     }
     
-    public int binarySearch(ArrayList<Integer> arr, int score){
-        int l=0, r=arr.size();
+    public int binarySearch(List<Integer> list, int score){
+        int s=0, e=list.size();
         
-        while(l<r){
-            int mid = (l+r)/2;
+        while(s<e){
+            int mid = (s+e)/2;
             
-            if(score<=arr.get(mid)){
-                r = mid;
+            if(list.get(mid)<score){
+                s = mid + 1;
             } else {
-                l = mid+1;
+                e = mid;
             }
         }
         
-        return arr.size()-l;
+        return list.size() - s;
     }
 }

3d85b3e0e5ea8f4319d9d35a7e37cdd61a370066
soyeon <109056397+thduss@users.noreply.github.com>
2026-09-12 11:17:27 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.53 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,33 +1,30 @@
-// 암호 해독 
-// 벽#, 공백 
-// 두 지도 겹쳐서 하나라도 벽이면 벽
-
-import java.util.*;
-
+/*
+- 공백 혹은 벽
+*/
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-
+        
+        int[] sum = new int[n];
         for(int i=0; i<n; i++){
-            String str1 = Integer.toBinaryString(arr1[i]);
-            String str2 = Integer.toBinaryString(arr2[i]);
+            sum[i] = arr1[i] | arr2[i];
+        }
+        
+        for(int i=0; i<n; i++){
+            String str = Integer.toBinaryString(sum[i]);
             
-            while (str1.length() < n) str1 = "0" + str1;
-            while (str2.length() < n) str2 = "0" + str2;
+            while(str.length()<n){
+                str = "0" + str;
+            }
             
             StringBuilder sb = new StringBuilder();
-            
-            for(int j=0; j<n; j++){
-                char c1 = str1.charAt(j);
-                char c2 = str2.charAt(j);
-                
-                if(c1=='1' || c2=='1'){
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j)=='1') {
                     sb.append("#");
                 } else {
                     sb.append(" ");
                 }
             }
-            
             answer[i] = sb.toString();
         }
         
